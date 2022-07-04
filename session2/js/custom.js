8b5420dc648a023845001b3785f4740e7426b29a
marwaMRadwan <marwaradwan872@hotmail.com>
2022-07-04 14:40:49 +0200

    s2

--- session2/js/custom.js
+++ session2/js/custom.js
@@ -1,63 +1,65 @@
 const addUser = document.querySelector("#addUser")
 const dataWrap = document.querySelector("#dataWrap")
-const readFromStorage = (keyword) =>{
+const singleUser = document.querySelector("#singleUser")
+const readFromStorage = (keyword, dataType="array") =>{
     let data 
     try{
         // data = localStorage.getItem("users") || []
         data = JSON.parse(localStorage.getItem(keyword))
-        if(!Array.isArray(data)) throw new Error("data must be an array")
+        if(!Array.isArray(data) && dataType=="array") 
+            throw new Error("data must be an array")
     }
     catch(e){
         data = []
     }
     return data
 }
-
 const writeDataToStorage = (keyword, data) => localStorage.setItem(keyword, JSON.stringify(data))
-
-const drawUsers = (allUsers) =>{
-    if(allUsers.length==0){
-        const tr = document.createElement('tr')
-        dataWrap.appendChild(tr)
-        tr.classList="alert alert-danger"
-        let td = document.createElement('td')
-        tr.appendChild(td)
-        td.setAttribute("colspan", 5)
-        td.textContent="no users yet"
-        td.classList="alert alert-danger text-center"
-    }
+const createMyOwnElement = (parent, ele, txt, classes,  attribute) =>{
+    // element, append in parent, class, textcontent, setattribute
+    const myEle = document.createElement(ele)
+    parent.appendChild(myEle)
+    if(classes) myEle.className= classes
+    if(txt) myEle.textContent=txt
+    if(attribute) myEle.setAttribute(attribute.name, attribute.val) // {name:"id", val:"1"}
+    return myEle
+}
+const emptyData = (msg, cols) =>{
+    const tr = createMyOwnElement(dataWrap, 'tr', null, 'alert alert-danger', null)
+    createMyOwnElement(tr, 'td', msg,'text-center', {name:"colspan", val:cols})
+}
+const deleteUser = (allUsers, ind) =>{
+    allUsers.splice(ind, 1)
+    writeDataToStorage("users", allUsers)
+    drawUsers(allUsers)
+}
+const editUser = (allUsers, ind) =>{ 
+}
+const showUser = (allUsers, ind) =>{
+    writeDataToStorage("single", {index: ind, userData: allUsers[ind]})
+    window.location.href="single.html"
+}
+const drawTable = (allUsers) => {
     allUsers.forEach((user, ind)=>{
-        const tr = document.createElement('tr')
-        dataWrap.appendChild(tr)
-        let td = document.createElement('td')
-        tr.appendChild(td)
-        td.textContent=ind+1
-        td = document.createElement('td')
-        tr.appendChild(td)
-        td.textContent=user.id
-        td = document.createElement('td')
-        tr.appendChild(td)
-        td.textContent=user.name
-        td = document.createElement('td')
-        tr.appendChild(td)
-        td.textContent=user.gender
-        td = document.createElement('td')
-        tr.appendChild(td)
-        const delBtn = document.createElement("button")
-        delBtn.className="btn btn-danger mx-2   "
-        delBtn.textContent="delete"
-        td.appendChild(delBtn)
-        const showBtn = document.createElement("button")
-        showBtn.className="btn btn-primary mx-2   "
-        showBtn.textContent="show"
-        td.appendChild(showBtn)
-        const editBtn = document.createElement("button")
-        editBtn.className="btn btn-warning mx-2   "
-        editBtn.textContent="edit"
-        td.appendChild(editBtn)
+        const tr = createMyOwnElement(dataWrap, 'tr', null, null, null)
+        createMyOwnElement(tr, 'td', ind+1, null, null)
+        createMyOwnElement(tr, 'td', user.id, null, null)
+        createMyOwnElement(tr, 'td', user.name, null, null)
+        createMyOwnElement(tr, 'td', user.gender, null, null)
+        const td = createMyOwnElement(tr, 'td', null, null, null)
+        const delBtn = createMyOwnElement(td, "button", 'delete', 'btn btn-danger mx-2', null)
+        delBtn.addEventListener("click", ()=> deleteUser(allUsers, ind))
+        const editBtn = createMyOwnElement(td, "button", 'edit', 'btn btn-warning mx-2', null)
+        editBtn.addEventListener("click", ()=> editUser(allUsers, ind))
+        const showBtn = createMyOwnElement(td, "button", 'show', 'btn btn-primary mx-2', null)
+        showBtn.addEventListener("click", ()=> showUser(allUsers, ind))
     })
 }
-
+const drawUsers = (allUsers) =>{
+    dataWrap.textContent=""
+    if(allUsers.length==0) emptyData("No Users Yet", 5)
+    else drawTable(allUsers)
+}
 if(addUser){
     addUser.addEventListener("submit", (e)=>{
         e.preventDefault()
@@ -73,8 +75,19 @@ if(addUser){
         window.location.href = "index.html"
     })
 }
-
 if(dataWrap){
     const allUsers= readFromStorage("users")
     drawUsers(allUsers)
+}
+if(singleUser){
+    const singleUserData = readFromStorage("single", "obj")
+    if(!singleUserData) {
+        const tr = createMyOwnElement(singleUser, 'div', "no users found", 'alert alert-danger', null)
+    }
+    const userData = singleUserData.userData
+    const userIndex = singleUserData.index
+    const tr = createMyOwnElement(singleUser, 'div', null, null, null)
+    createMyOwnElement(tr, 'h4', userData.id, null, null)
+    createMyOwnElement(tr, 'h4', userData.name, null, null)
+    createMyOwnElement(tr, 'h4', userData.gender, null, null)
 }
