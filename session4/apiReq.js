e9ad1eb239f97a2d4685dbb50155e3cd9987ea30
marwaMRadwan <marwaradwan872@hotmail.com>
2022-07-06 11:35:02 +0200

    s4

--- session4/apiReq.js
+++ session4/apiReq.js
@@ -1,14 +1,40 @@
 // http http2 https  node-fetch ....
 
 const https = require("https")
+const yargs = require("yargs")
 const url = "https://jsonplaceholder.typicode.com/users"
 
-const req = https.request(url , (res)=>{
-    let myResult = ""
-    res.on("data", x=>myResult+=x.toString())
-    res.on("end", ()=> console.log(JSON.parse(myResult)))
+// const req = https.request(url , (res)=>{
+//     let myResult = ""
+//     res.on("data", x=>myResult+=x.toString())
+//     res.on("end", ()=> console.log(JSON.parse(myResult)))
+// })
+// req.on("error", (err)=>{
+//     console.log(err)
+// })
+// req.end()
+
+const apiRequest = (url, cb) =>{
+    const req = https.request(url , (res)=>{
+        let myResult = ""
+        res.on("data", x=>myResult+=x.toString())
+        res.on("end", ()=> cb(false , JSON.parse(myResult)))
+    })
+    req.on("error", (err)=> cb(err, false))
+    req.end()
+}
+
+yargs.command({
+    command:"req",
+    builder:{
+        url: {type:"string", demandOption:true}
+    },
+    handler:(argv)=>{
+        console.log("test")
+        apiRequest(argv.url, (err, result)=>{
+            if(err) return console.log(err)
+            console.log(result)
+        })
+    }
 })
-req.on("error", (err)=>{
-    console.log(err)
-})
-req.end()
+yargs.argv
