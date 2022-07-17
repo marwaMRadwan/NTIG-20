1308b59752ff647380f25a26812d1dd59bf304f2
marwaMRadwan <marwaradwan872@hotmail.com>
2022-07-17 10:24:31 +0200

    s5

--- session5/app/src.js
+++ session5/app/src.js
@@ -1,8 +1,16 @@
 const express = require("express")
 const hbs = require("hbs")
 const path = require("path")
-
 const app = express()
 
+app.use(express.static(path.join(__dirname, "../resources/public")))
+
+app.set("view engine", "hbs")
+app.set("views" , path.join(__dirname, "../resources/views"))
+
+hbs.registerPartials(path.join(__dirname, "../resources/layouts"))
+
+const userRoutes = require("../routes/user.routes")
+app.use(userRoutes)
 
 module.exports = app
