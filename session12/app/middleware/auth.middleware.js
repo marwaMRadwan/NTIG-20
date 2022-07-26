a84dbbc521ab30b0f0106b0187d8e3e8c66877ec
marwaMRadwan <marwaradwan872@hotmail.com>
2022-07-26 14:51:05 +0200

    s13.

--- session12/app/middleware/auth.middleware.js
+++ session12/app/middleware/auth.middleware.js
@@ -1,12 +1,16 @@
 const {resGenerator} = require("../helpers/methods")
 const userModel = require("../db/models/user.model")
 const jwt = require("jsonwebtoken")
+const checkUser = async(token)=>{
+    const decoded = jwt.verify(token, process.env.JWTKEY)
+    const user = await userModel.findOne({_id: decoded._id, "tokens.token": token})
+    if(!user) throw new Error("user not found")
+    return user
+}
 const auth = async(req, res, next)=>{
     try{
         const token = req.header("Authorization").replace("Bearer ", "")
-        const decoded = jwt.verify(token, process.env.JWTKEY)
-        const user = await userModel.findOne({_id: decoded._id, "tokens.token": token})
-        if(!user) throw new Error("user not found")
+        const user = await checkUser(token)
         req.user = user
         req.token = token
         next()
@@ -16,4 +20,18 @@ const auth = async(req, res, next)=>{
     }
 }
 
-module.exports=auth
+const authAdmin = async(req, res, next)=>{
+    try{
+        const token = req.header("Authorization").replace("Bearer ", "")
+        const user = checkUser(token)
+        if(user.type != "admin") throw new Error("you are not admin")
+        req.user = user
+        req.token = token
+        // res.send(req.user)
+        next()
+    }
+    catch(e){
+        resGenerator(res, 500, e.message, "unauthorized")
+    }
+}
+module.exports={auth, authAdmin}
--- session12/app/routes/user.routes.js
+++ session12/app/routes/user.routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 const user = require("../controllers/user.controller")
-const auth = require("../middleware/auth.middleware")
+const {auth, authAdmin} = require("../middleware/auth.middleware")
 router.post("/register", user.register)
 router.post("/login", user.login)
 router.post("/activateAcc", user.activate)
